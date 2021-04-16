6d3ee78b5e9cb897c9c13b5ef17f5aaa6713d919
Kartik Agashe <kartikagashe30@gmail.com>
2021-04-16 14:39:18 +0530

    approved not shown to superverifier; generated pdf changes -16/04/2021

--- src/main/java/com/scube/edu/repository/VerificationRequestRepository.java
+++ src/main/java/com/scube/edu/repository/VerificationRequestRepository.java
@@ -41,7 +41,7 @@ public interface VerificationRequestRepository extends JpaRepository<Verificatio
 	@Query(value = "SELECT * FROM verification_request where user_id = (?1) and convert(created_date , Date) >= (?2) and convert(created_date , Date) <= (?3)" , nativeQuery = true)
 	List<VerificationRequest> findByUserIdAndDates(long userId, String fromDate, String toDate);
 
-	@Query(value = "SELECT * FROM verification_request where doc_status in ('Rejected', 'Unable To Verify', 'Offline','Approved') and convert(created_date , Date) >= (?1) and convert(created_date , Date) <= (?2)" , nativeQuery = true)
+	@Query(value = "SELECT * FROM verification_request where doc_status in ('Rejected', 'Unable To Verify', 'Offline') and convert(created_date , Date) >= (?1) and convert(created_date , Date) <= (?2)" , nativeQuery = true)
 	List<VerificationRequest> findByStatus(String fromDate, String toDate);
 	
 
