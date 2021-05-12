ee76a82067491a8acbb05ed3ccf6048cd8e37b6b
Kartik Agashe <kartikagashe30@gmail.com>
2021-05-12 12:45:11 +0530

    deleteMapping annotation added to delete API for branch -12/05/2021

--- src/main/java/com/scube/edu/controller/BranchMasterController.java
+++ src/main/java/com/scube/edu/controller/BranchMasterController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -134,7 +135,7 @@ public class BranchMasterController {
 		
 	}
 	
-	@PostMapping("/deleteBranch/{id}")
+	@DeleteMapping("/deleteBranch/{id}")
 	public ResponseEntity<Object> deleteBranch(@PathVariable Long id, HttpServletRequest request) {
 		
 		response = new BaseResponse();
