ef6bbb975946d87b2e5f988f31cf4fe0934d5f93
manishap31 <manishap31@outlook.com>
2021-05-28 18:28:03 +0530

    year of passing APIs implemented -28/05/2021

---
+++ src/main/java/com/scube/edu/request/YearOfPassingRequest.java
@@ -0,0 +1,15 @@
+package com.scube.edu.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter 
+@Setter
+@ToString
+public class YearOfPassingRequest {
+	
+	private Long id;
+	private String yearOfPass;
+
+}
--- src/main/java/com/scube/edu/service/YearOfPassingService.java
+++ src/main/java/com/scube/edu/service/YearOfPassingService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import com.scube.edu.model.PassingYearMaster;
+import com.scube.edu.request.YearOfPassingRequest;
+import com.scube.edu.response.BaseResponse;
 import com.scube.edu.response.YearOfPassingResponse;
 
 public interface YearOfPassingService {
@@ -12,6 +14,12 @@ public interface YearOfPassingService {
 	List<YearOfPassingResponse> getYearOfPassingList(HttpServletRequest request);
 
 	PassingYearMaster getYearById(String id);
+
+	boolean addYear(YearOfPassingRequest yearOfPass) throws Exception;
+
+	boolean deleteYear(long id) throws Exception;
+
+	String updateYearById(YearOfPassingRequest yearOfPassReq, HttpServletRequest request) throws Exception;
 
 
 }
